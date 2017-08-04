8297964b8cd1e12df66d3e4b56a6c72c93630ea2
Joseph Gordy <jgordy2424@gmail.com>
2017-08-04 09:17:00 -0400

    finished styling

---
+++ main.js
@@ -0,0 +1,50 @@
+// Writing the JavaScript and interacting with the DOM is the main part of the project
+// Start small by selecting some elements
+let keys = document.getElementsByTagName("span");
+let operators = ["+","-","x","/"];
+let decimalAdded = false;
+
+console.log(keys);
+
+// Need event listener for each button
+
+for (var i = 0; i < keys.length; i++) {
+  keys[i].onclick = function(); {
+
+// Need variables for inputs
+  let calcScreen = document.querySelector('.screen');
+		let screenVal = input.innerHTML;
+		let btnVal = this.innerHTML;
+  }
+
+// if statment for operators
+  if () {
+
+  }
+
+
+}
+
+// What algorithms do you need to implement?
+
+
+
+
+
+
+// Create some functions to store repeatable steps
+
+
+
+
+
+
+
+// You'll need to find a way to track the numbers pressed until the = is pressed
+
+
+
+
+
+
+// No need for chaining long calculations yet (see hard mode)

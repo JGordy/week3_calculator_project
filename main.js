d64a7e052ff680eb9b40a450a1be53750790cb83
Joseph Gordy <jgordy2424@gmail.com>
2017-08-06 14:04:16 -0400

    Squashed some bugs

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ let para = document.getElementsByClassName("para");
 
 // added Event Listeners to clear and eval
 
-// add flag for multiple operators?
 let clear = document.getElementById("clear");
 clear.addEventListener("click", clearClicked);
 
@@ -20,13 +19,17 @@ modulo.addEventListener("click", modClicked);
 let root = document.getElementById("root");
 root.addEventListener("click", rootClicked);
 
-
 // funtions for operators
 
 function operatorClicked(event) {
 
   if (para.textContent === "") {
     para.textContent = "";
+  } else if ((para.textContent.endsWith("+")) ||
+              (para.textContent.endsWith("-")) ||
+              (para.textContent.endsWith("*")) ||
+              (para.textContent.endsWith("/"))) {
+      para.textContent = para.textContent;
   } else if (event.target.innerHTML === "X") {
     para.textContent += " " + "*" + " ";
   } else {
@@ -39,15 +42,26 @@ function modClicked(event) {
 }
 
 function rootClicked(event) {
-  para.textContent += " " + event.target.innerHTML + " " + "=" + " " + Math.sqrt(para.textContent).toFixed(6);
+
+if (Math.sqrt(para.textContent) != parseInt(Math.sqrt(para.textContent))) {
+
+    para.textContent += " " + event.target.innerHTML + " " + "=" + " " + Math.sqrt(para.textContent).toFixed(6);
+
+} else {
+
+  para.textContent += " " + event.target.innerHTML + " " + "=" + " " + Math.sqrt(para.textContent)
 }
-
-
+}
 
 // Number and decimal function
 
 function numberClicked(event) {
-  para.textContent += event.target.innerHTML;
+
+  if (para.textContent.includes("=")) {
+    // do nothing
+  } else {
+    para.textContent += event.target.innerHTML;
+  }
 }
 
 // Clear function
@@ -68,7 +82,12 @@ function clearClicked(event) {
 function evalClicked(event) {
 
   //  use create element, add text to element, appendChild to display to get history and scrolling
-  para.textContent = para.textContent + " " + "=" + " " + eval(para.textContent);
+  if (para.textContent == "") {
+    // do nothing
+  } else {
+      para.textContent = para.textContent + " " + "=" + " " + eval(para.textContent);
+  }
+
 }
 
 // add event listeners to keys and operators
